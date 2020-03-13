869ddd086ff657551b8a0266c989b9163d0764f3
Alex Bosworth <alex.bosworth@gmail.com>
2020-03-13 15:29:43 -0700

    async find maximum

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const getMaximum = require('./get_maximum');
 const returnResult = require('./return_result');
 
 /** Async utilities
 */
-module.exports = {returnResult};
+module.exports = {getMaximum, returnResult};
